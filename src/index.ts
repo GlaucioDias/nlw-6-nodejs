afabe1d1ed480b30127e2668a293e1dd98ffc2e8
Glaucio Dias <fleevrl@gmail.com>
2021-06-22 00:09:10 -0300

    Instalando e configurando typeorm com sqlite3

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
 import express, { NextFunction, Request, Response} from 'express';
-
+import './database'
 
 const PORT = process.env.PORT || 3300
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.status(200).json({ message: 'Ola NLW'})
-})
-
-app.post('/test-post', (req, res) => {
-    return res.status(200).json({ message: 'test-post'})
-})
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
