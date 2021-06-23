425adec5c7650fc275330dd1068082d56b48692c
GlaucioDias <fleevrl@gmail.com>
2021-06-22 22:07:06 -0300

    Criação de rotas

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-import express, { NextFunction, Request, Response} from 'express';
+import express from 'express';
+import { router } from './routes';
 import './database'
 
 const PORT = process.env.PORT || 3300
 
 const app = express();
+
+app.use(express.json())
+
+app.use(router)
 
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
